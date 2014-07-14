040f8bdf9323df06099f102fbfb636e47e1c2939
Eric <ericfode@gmail.com>
2014-07-13 23:40:04 -0700

    Included url-encoded for twilio verificion of header. Also changed indexs string to something more awesome.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
 var express = require('express');
-var _ = require('underscore');
 var router = express.Router();
-var client = require('twilio')(process.env.AUTH_KEY, process.env.SECRET);
 /* GET home page. */
 
-function fizzbuzz(upto) {
-    array = _.range(0, upto, 1);
-}
 
 router.get('/', function (req, res) {
-    res.render('index', { title: process.env.AUTH_KEY });
+    res.render('index', { title: 'Fancy thing to show off awesome' });
 });
 
 module.exports = router;
--- routes/twiml.js
+++ routes/twiml.js
@@ -3,25 +3,29 @@
  *
  * Created by Eric on 7/13/2014.
  */
-var _ = require('underscore');
-var express = require('express');
-var router = express.Router();
-var twilio = require('twilio')(process.env.AUTH_KEY, process.env.SECRET);
+var _ = require('underscore'),
+    express = require('express'),
+    router = express.Router(),
+    twilio = require('twilio')(process.env.AUTH_KEY, process.env.SECRET);
 
+var opts = { host: 'https://twilio-lendup.herokuapp.com'};
+var auth_token = process.AUTH_KEY;
 
-router.post('/call', twilio.webhook({
-    host: 'https://twilio-lendup.herokuapp.com'
-}), function (req, res) {
-    var twiml = new twilio.TwimlResponse();
-    twiml.message("This is a test message");
-    res.send(twiml);
+router.post('/call', function (req, res) {
+    if (twilio.validateExpressRequest(req, auth_token, opts)) {
+        var twiml = new twilio.TwimlResponse();
+        twiml.message("This is a test message");
+        res.send(twiml);
+    }
 });
 
 
-router.post('/text', twilio.webhook({
-    host: 'https://twilio-lendup.herokuapp.com'
-}), function (req, res) {
-    var twiml = new twilio.TwimlResponse();
-    twiml.message("This is a test message");
-    res.send(twiml);
+router.post('/text', function (req, res) {
+    if (twilio.validateExpressRequest(req, auth_token, opts)) {
+        var twiml = new twilio.TwimlResponse();
+        twiml.message("This is a test message");
+        res.send(twiml);
+    }
 });
+
+module.exports = router;
