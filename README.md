882ea4b3b1b0dd5812d1990854f189e2aa781592
Eric <ericfode@gmail.com>
2014-07-14 04:05:15 -0700

    Removeing timer... So delay fail

---
+++ README.md
@@ -0,0 +1,12 @@
+Quick summary of what is done
+
+You can make calls to a number
+The fizzbuzz prompt works (mounted at /twiml/call)
+Fizzbuzz works
+Calls get stored in mongodb
+Delays kinda work
+
+
+Things that could be better...
+Well the request logic is rather untested
+The delay will break if the server goes down
--- routes/index.js
+++ routes/index.js
@@ -52,24 +52,23 @@ router.post('/start_call', function (req, res) {
     });
     c.save();
 
-    setInterval(when, function () {
-        var promise = tclient.makeCall({
-            to: req.param(['number']),
-            from: number,
-            url: 'https://twilio-lendup.herokuapp.com/twiml/call'
-        });
-
-        promise.then(function (call) {
-            c.sid = call.sid;
-            c.status = "In progress";
-            c.save();
-            console.log('Call success! Call SID: ' + call.sid);
-        }, function (error) {
-            console.error('Call failed!  Reason: ' + error.message);
-            c.status = "Failed" + error.message;
-            c.save();
-        });
+    var promise = tclient.makeCall({
+        to: req.param(['number']),
+        from: number,
+        url: 'https://twilio-lendup.herokuapp.com/twiml/call'
     });
+
+    promise.then(function (call) {
+        c.sid = call.sid;
+        c.status = "In progress";
+        c.save();
+        console.log('Call success! Call SID: ' + call.sid);
+    }, function (error) {
+        console.error('Call failed!  Reason: ' + error.message);
+        c.status = "Failed" + error.message;
+        c.save();
+    });
+
     res.send("job Queued");
 });
 
