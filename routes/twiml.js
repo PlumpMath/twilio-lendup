288a1540f4c0678896fc7ac27bad9dbf63739347
Eric <ericfode@gmail.com>
2014-07-14 00:25:21 -0700

    Changing back to twilio webhook

--- routes/twiml.js
+++ routes/twiml.js
@@ -11,25 +11,18 @@ var _ = require('underscore'),
 var opts = { host: 'https://twilio-lendup.herokuapp.com'};
 var auth_token = process.env.AUTH_KEY;
 
-router.post('/call', function (req, res) {
-    if (twilio.validateExpressRequest(req, auth_token, opts)) {
-        var twiml = new twilio.TwimlResponse();
+router.post('/call', twilio.webhook(opts, function (req, res) {
+    var twiml = new twilio.TwimlResponse();
         twiml.message("This is a test message");
         res.send(twiml);
-    } else {
-        res.render('Herp derp')
-    }
-});
+}));
 
 
-router.post('/text', function (req, res) {
-    if (twilio.validateExpressRequest(req, auth_token, opts)) {
-        var twiml = new twilio.TwimlResponse();
+router.post('/text', twilio.webhook(opts, function (req, res) {
+    var twiml = new twilio.TwimlResponse();
         twiml.message("This is a test message");
         res.send(twiml);
-    } else {
-        res.render('Herp derps')
-    }
-});
+}));
+
 
 module.exports = router;
